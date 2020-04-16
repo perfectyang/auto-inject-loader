a78268222e49b9e31f7119df2ba8ca51aac52baf
杨国唯 <yangguowei@yangguoweideMac-mini.local>
2020-04-16 17:52:50 +0800

    去除log

--- src/transt.js
+++ src/transt.js
@@ -8,7 +8,6 @@ const {isIncludes} = require('./utils')
 function generateDeclaration (url, names) {
   const temArr = []
   names.forEach(cur => {
-    console.log('bllss要------', cur)
     if (cur.key === 'normal') {
       temArr.push(
         t.importSpecifier(t.identifier(cur.name), t.identifier(cur.name))
@@ -56,87 +55,5 @@ function transtJs (code, options) {
   }
   return generate(ast).code
 }
-
-// const testTpl = `
-// import surveyApi from '@/api/survey'
-// export default {
-//   name: 'outline',
-//   components: {
-//     datas: () => import('./datas.vue'),
-//     ProjectHeader: () => import('index/component/project-header.vue'),
-//     dragQs: () => import('index/component/drag-qs/index.vue'),
-//     importOutline: () => import('index/component/import-outline/index.vue')
-//   },
-//   provide () {
-//     return {
-//       parentDatas: this
-//     }
-//   },
-//   data () {
-//     return {
-//       surveyInfo: {},
-//       surveyList: [],
-//       originList: [],
-//       test: getUserAvatorBackgroundColor(),
-//       loading: false
-//     }
-//   },
-//   created () {
-//     myDefalult
-//     this.highFnSurveyView()
-//     myDefalult
-//     getProjectPath('a')
-//   },
-//   computed: {
-//     exportOutLine () {
-//       myDefalult
-//       getEnvStaticPath.on('emit')
-//       return link
-//     }
-//   },
-//   methods: {
-//     success (file) {
-//       hasUsableFlash
-//       console.log('aa', file)
-//     },
-//     importOutLine () {
-//       this.$refs.importOutline.dialogVisible = true
-//     },
-//     highFnSurveyView () {
-//       this.loading = true
-//       this.surveyView().then(rs => (this.loading = false), reason => (this.loading = false))
-//       const getEnvHostname = '1111'
-//     },
-//     surveyView () {
-//       return surveyApi.surveyView({
-//         project_id: getQueryString('project_id'),
-//         is_update_redis: 1
-//       }).then(rs => {
-//         this.surveyInfo = rs.project
-//         this.originList = rs.list
-//         this.surveyList = toTwoArr(rs.list)
-//         return rs
-//       }, reason => {
-//         return Promise.reject(null)
-//       })
-//     }
-//   }
-// }
-
-// `
-
-// const newCode = transtJs(testTpl, {
-//   autoImport: [
-//     {
-//       basePath: path.join(__dirname, '../'),
-//       paths: 'utils/index.js'
-//     },
-//     {
-//       basePath: path.join(__dirname, '../'),
-//       paths: 'utils/env.js'
-//     }
-//   ]
-// })
-// console.log('newCode', newCode)
 
 module.exports = transtJs
